f4b9491c083d7fdc8e79a5a91ec8c87d8dd7f7b7
Raju Yadav <yrajukumar47@gmail.com>
2025-11-03 21:55:22 -0800

    updated blog page

---
+++ sanity/schema.ts
@@ -0,0 +1,6 @@
+import { type SchemaTypeDefinition } from 'sanity'
+import { schemaTypes } from './schemaTypes'
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: schemaTypes,
+}
