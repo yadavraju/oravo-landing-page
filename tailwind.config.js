ff5af894fad54f2f7ff297b7eb2f0f6164a47806
Raju Yadav <yrajukumar47@gmail.com>
2025-12-31 13:37:47 -0800

    updated ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+    },
   },
   plugins: [],
   corePlugins: {
